e1e27a0223b7a2f4e37023e3a5d837b1aaea25ca
Emix256 <133218243+Emix256@users.noreply.github.com>
2023-05-17 19:16:19 +0200

    Update glos.js

--- js/glos.js
+++ js/glos.js
@@ -2,7 +2,7 @@ let x;
 
 x = document.getElementById("zdanie").value;
 document.getElementById("btn").onclick = function(){
-  var msg = x;
+  var msg = "hello! Am i working?";
   const utterance = new SpeechSynthesisUtterance(msg);
   SpeechSynthesis.speak(utterance);
 }
