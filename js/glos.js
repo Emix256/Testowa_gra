f95a7af7f32b7c1e0ce044705bb02f42408acd5d
Emix256 <133218243+Emix256@users.noreply.github.com>
2023-05-19 12:25:37 +0200

    Update glos.js

--- js/glos.js
+++ js/glos.js
@@ -1,6 +1,12 @@
-let x = document.getElementById("zdanie").value;
+const przycisk = document.getElemntById("btn")
+const tekst = document.getElementById("zdanie")
 
-document.getElementById("btn").onclick = function(){
-  let utterance = new SpeechSynthesisUtterance(x);
-  SpeechSynthesis.speak(utterance);
+przycisk.addEventListener('click', () => {
+  playText(tekst.value)
+})
+
+function playText(zdanie) {
+  const utterance = new SpeechSynthesistUtterance(zdanie)
+  utterance.rate = 1
+  speechSynthesist.speak(utterance)
 }
